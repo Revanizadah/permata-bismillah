1b66d810cfe690833be9ea367d45436649e5aa24
rivalramadhan <ripallez@gmail.com>
2025-05-14 18:35:42 +0700

    refactor: clean up LapanganController and simplify index view

--- app/Http/Controllers/LapanganController.php
+++ app/Http/Controllers/LapanganController.php
@@ -2,39 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Lapangan;  // Correct namespace for Lapangan model
+use App\Models\Lapangan;
 use Illuminate\Http\Request;
 
 class LapanganController extends Controller
 {
-    // Display a listing of lapangan
     public function index()
     {
-        // Get all lapangan records
-        $lapangan = Lapangan::all();
-        return view('lapangan.index', compact('lapangan'));
-    }
+        $lapangans = Lapangan::all();
 
-    public function futsal()
-    {
-        // Return the futsal view from the 'futsal' folder
-        return view('futsal.index');  // views/futsal/index.blade.php
+        return view('lapangan.index', compact('lapangans'));
     }
-
-    // Method to handle the Badminton page
-    public function badminton()
-    {
-        // Return the badminton view from the 'badminton' folder
-        return view('badminton.index');  // views/badminton/index.blade.php
-    }
-
-    // Show the form for creating a new lapangan
     public function create()
     {
         return view('lapangan.create');
     }
-
-    // Store a newly created lapangan in the database
     public function store(Request $request)
     {
         // Validate the request data
