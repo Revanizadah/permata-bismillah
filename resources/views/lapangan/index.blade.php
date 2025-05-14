5cd9329d09652062590d19b12364e6c1b951a3a4
Revaniza <revanizadah.1404@gmail.com>
2025-05-14 21:44:00 +0700

    refactor: update index view for lapangan to use Blade templating and improve layout

--- resources/views/lapangan/index.blade.php
+++ resources/views/lapangan/index.blade.php
@@ -1,33 +1,29 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Users List</title>
-</head>
-<body>
-    <h1>Users List</h1>
-    <table>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h2>Daftar Lapangan</h2>
+    <table class="table table-striped">
         <thead>
             <tr>
-                <th>Id</th>
                 <th>Nama Lapangan</th>
-                <th>harga perjam</th>
-                <th>jenis lapangan</th>
-                <th>status</th>
-                <th>deskripsi</th>
+                <th>Jenis Lapangan</th>
+                <th>Harga per Jam</th>
+                <th>Status</th>
+                <th>Deskripsi</th>
             </tr>
         </thead>
         <tbody>
-            @foreach ($lapangans as $lapangan)
+            @foreach($lapangans as $lapangan)
                 <tr>
-                    <td>{{ $lapangan->id }}</td>
                     <td>{{ $lapangan->nama_lapangan }}</td>
-                    <td>{{ $lapangan->harga_per_jam }}</td>
                     <td>{{ $lapangan->jenis_lapangan }}</td>
+                    <td>Rp {{ number_format($lapangan->harga_per_jam, 0, ',', '.') }}</td>
                     <td>{{ $lapangan->status }}</td>
                     <td>{{ $lapangan->deskripsi }}</td>
                 </tr>
             @endforeach
         </tbody>
     </table>
-</body>
-</html>
+</div>
+@endsection
