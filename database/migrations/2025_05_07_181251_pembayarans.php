1f00c6fb3fee42eff7a938eb03a33d0a4b408a73
rivalramadhan <ripallez@gmail.com>
2025-05-29 12:20:24 +0700

    feat: add expired_at field to pembayarans migration for payment expiration tracking

--- database/migrations/2025_05_07_181251_pembayarans.php
+++ database/migrations/2025_05_07_181251_pembayarans.php
@@ -22,6 +22,7 @@ return new class extends Migration
             $table->string('bukti_pembayaran')->nullable();
             $table->string('status', 20)->default('Pending');
             $table->text('catatan')->nullable();
+            $table->dateTime('expired_at')->nullable();
             $table->timestamps();
         });
 
