a3ef06cdc84934138c1007d6464ef02802079dc8
rivalramadhan <ripallez@gmail.com>
2025-06-04 16:11:37 +0700

    feat: implement user management features with index, create, store, show, edit, and update routes; update sidebar and views for admin consistency

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
     public function index()
     {
         $users = User::all();
-        return view('users.index', compact('users'));
+        return view('users.index-admin', compact('users'));
     }
 
     // Tampilkan form tambah user
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,13 @@ Route::resource('lapangan', LapanganController::class);
 
 // CRUD User
 Route::resource('users', UserController::class);
+Route::get('user', [UserController::class, 'index'])->name('user.index');
+Route::get('user/create', [UserController::class, 'create'])->name('user.create');
+Route::post('user', [UserController::class, 'store'])->name('user.store');
+Route::get('user/{user}', [UserController::class, 'show'])->name('user.show');
+Route::get('user/{user}/edit', [UserController::class, 'edit'])->name('user.edit');
+Route::put('user/{user}', [UserController::class, 'update'])->name('user.update');
+Route::delete('user/{user}', [UserController::class, 'destroy'])->name('user.destroy');
 
 // Route pembayaran
 Route::get('pembayaran', [PembayaranController::class, 'index'])->name('pembayaran.index');
