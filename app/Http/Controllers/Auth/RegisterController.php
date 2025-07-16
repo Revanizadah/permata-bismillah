c0d6f1469f3636001448ced7b5d8b576b498878e
rivalramadhan <ripallez@gmail.com>
2025-07-17 01:51:46 +0700

    refactor: add LoginController and RegisterController with authentication logic

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules;
+
+class RegisterController extends Controller
+{
+    public function create()
+    {
+        return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'nama' => $request->nama,
+            'email' => $request->email,
+            'password' => $request->password, // Password akan di-hash otomatis oleh Model
+        ]);
+
+        // Langsung login setelah registrasi
+        Auth::login($user);
+
+        // Arahkan ke dashboard setelah berhasil
+        return redirect()->route('admin.dashboard');
+    }
+}
